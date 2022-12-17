09dd89743029256309e7e5c95c17183c366451c3
Hans Ellis <bullhorn@gmail.com>
2022-12-17 10:59:33 -0800

    implement edit functionality

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -37,5 +37,19 @@ namespace ToDoList.Controllers
       Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
+
+    public ActionResult Edit (int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Items.Update(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
